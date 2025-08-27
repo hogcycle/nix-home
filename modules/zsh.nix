23ad915d62f265b2606e9b8133f58beffabdfd53
hogcycle <nate@gysli.ng>
2025-08-27 01:52:27 -0400

    zsh

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -2,7 +2,18 @@
 
   programs.zsh = {
     enable = true; 
-  }; 
+    initExtra = ''
+	export EDITOR="nvim"
+
+	function vim() {
+	    if [ -z "$@" ]; then
+		nvim .
+	    else
+		nvim $@
+	    fi
+	}
+    ''; 
+  };
   programs.starship = { 
     enable = true; 
   }; 
