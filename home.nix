f982e49ab30d0bb62ef06267895670fd0b1c265e
hogcycle <nate@gysli.ng>
2025-08-26 22:15:54 -0400

    remove redundant kanshi entry

--- home.nix
+++ home.nix
@@ -35,45 +35,6 @@ qt = {
   enable = true;
   platformTheme.name = "gtk";
 };
- services.kanshi = { 
-	enable = true;  
-    profiles = {
-      docked = {
-        outputs = [
-          { criteria = "eDP-1"; 
-			status = "disable"; 
-		  }
-          { criteria = "DP-7"; 
-			status = "enable";
-			mode = "3840x2160@60Hz"; 
-			position = "0,0"; 
-			scale = 1.5; }
-        ];
-      };
-      laptop = {
-        outputs = [
-          { criteria = "eDP-1";
-			 status = "enable";
-			 mode = "2160x1350@59.74Hz";
-			 position = "0,0";
-			 scale = 1.5;
-			 }
-          { criteria = "DP-7";
-			 status = "disable"; }
-        ];
-      };
-
-      default = {
-        outputs = [
-          { criteria = "eDP-1";
-			 status = "enable";
-			 #mode = "2160x1350@59.74Hz";
-			 position = "0,0";
-			 scale = 1.5; }
-        ];
-      };
-    };
-  };	
   xdg.portal.enable = true; 
   xdg.portal.extraPortals = [
     pkgs.xdg-desktop-portal-gtk 
