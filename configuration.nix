9c33b54de0fc6d2765edceae334622946f26a4d9
hogcycle <nate@gysli.ng>
2025-09-04 13:04:43 -0400

    changed tailscale fix to overlay; added fonts

--- configuration.nix
+++ configuration.nix
@@ -4,19 +4,20 @@
 
 { config, lib, pkgs, ... }:
 {
-   # temp patch for tailscale to work
-    boot.kernelPatches = [
-      # Fix the /proc/net/tcp seek issue
-      # Impacts tailscale: https://github.com/tailscale/tailscale/issues/16966
-      {
-        name = "proc: fix missing pde_set_flags() for net proc files";
-        patch = pkgs.fetchurl {
-          name = "fix-missing-pde_set_flags-for-net-proc-files.patch";
-          url = "https://patchwork.kernel.org/project/linux-fsdevel/patch/20250821105806.1453833-1-wangzijie1@honor.com/raw/";
-          hash = "sha256-DbQ8FiRj65B28zP0xxg6LvW5ocEH8AHOqaRbYZOTDXg=";
-        };
-      }
-    ];
+  nixpkgs.overlays = [
+  (_: prev: {
+  tailscale = prev.tailscale.overrideAttrs (old: {
+    checkFlags =
+      builtins.map (
+        flag:
+          if prev.lib.hasPrefix "-skip=" flag
+          then flag + "|^TestGetList$|^TestIgnoreLocallyBoundPorts$|^TestPoller$"
+          else flag
+      )
+      old.checkFlags;
+  });
+})
+]; 
   imports = [
       ./hardware-configuration.nix
   ];
@@ -44,11 +45,15 @@
         enable = true; 
 	defaultFonts = {
 	  monospace = [ "JetBrainsMono Nerd Font Mono" "JetBrains Mono" ]; 
+	  serif = [ "Liberation Serif" ]; 
+	  sansSerif = [ "Liberation Sans" ]; 
         }; 
       }; 
       packages = with pkgs; [
 	nerd-fonts.jetbrains-mono
 	nerd-fonts.fira-code
+	noto-fonts-color-emoji
+	liberation_ttf
       ]; 
   }; 
   xdg.portal = {
