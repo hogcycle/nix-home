3d097a85d989f89cbd6437d4e73964f779749801
hogcycle <nate@gysli.ng>
2025-10-20 23:49:59 -0400

    ssh shortcuts

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -4,6 +4,9 @@
     enable = true; 
     shellAliases = { 
       vim = "nvim";
+	  repono = "kitty +kitten ssh -i ~/.ssh/homelab nate@192.168.1.140"; 
+	  udelgo = "kitty +kitten ssh -i ~/.ssh/udel-go ngysling@go.eecis.udel.edu"; 
+	  taildrop = "sudo tailscale file get .";
     };
     initContent = "ssh-add ~/.ssh/github 2>/dev/null || true"; 
   }; 
